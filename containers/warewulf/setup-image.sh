a339d1b5dc23e0bf7f314bcb24b15778924f04f5
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2025-11-08 03:44:33 +0000

    Cleanup image build process (build order)

--- containers/warewulf/setup-image.sh
+++ containers/warewulf/setup-image.sh
@@ -18,6 +18,14 @@ dnf install -y dnf-utils epel-release rocky-release-rpi
 dnf config-manager --set-enabled crb
 dnf update -y
 
+## Install and configure dracut and dependencies first (reduce build time)
+dnf install -y dracut ignition gdisk
+echo 'hostonly="no"' > /etc/dracut.conf.d/wwinit.conf
+echo 'add_dracutmodules+=" wwinit ignition "' >> /etc/dracut.conf.d/wwinit.conf
+
+## Install warewulf-dracut
+dnf install -y https://github.com/warewulf/warewulf/releases/download/v${WW_VERSION}/warewulf-dracut-${WW_VERSION}-1.EL${OS_RELEASE}.noarch.rpm
+
 ## Install RPi kernel
 dnf install -y kernel-rpi-4k-core kernel-rpi-firmware rpi-firmware-bluez rpi-firmware-nonfree
 
@@ -28,26 +36,12 @@ dnf install -y \
   openssh openssh-clients openssh-server iproute iputils NetworkManager sudo \
   nano vim less bash-completion man
 
-## Configure system
+## Configure boot
+chmod 644 /boot/efi/initramfs8
+
+## Configure login
 passwd -d root
 sed -i '/alias/d' /root/.bashrc
-EOF
-
-## Setup dracut for disk provisioning
-wwctl image exec nodeimage --build=false -- /bin/bash -ex <<EOF
-## Install Dracut
-dnf install -y https://github.com/warewulf/warewulf/releases/download/v${WW_VERSION}/warewulf-dracut-${WW_VERSION}-1.EL${OS_RELEASE}.noarch.rpm
-
-# configure dracut for provisoin to disk
-echo 'hostonly="no"' > /etc/dracut.conf.d/wwinit.conf
-echo 'add_dracutmodules+=" wwinit ignition "' >> /etc/dracut.conf.d/wwinit.conf
-
-# install disk tools
-dnf install -y ignition gdisk
-
-# build dracut
-dracut --force /boot/efi/initramfs8
-chmod 644 /boot/efi/initramfs8
 EOF
 
 ## Build image
